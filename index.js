c307bf64d20e9c60383e73b929d16fe7f3e56d08
Pauline Moraa <pmoraa431@gmail.com>
2025-03-16 00:59:29 +0300

    Moving Things with JavaScript by Acting on Events

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Your code here
 const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
